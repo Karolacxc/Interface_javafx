4eb606c207c87c1a753efac828b17066a14f0d41
fahanny <flayrahanny@hotmail.com.br>
2023-06-08 16:31:01 -0300

    a

--- src/data/DataCachorro.java
+++ src/data/DataCachorro.java
@@ -14,10 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 import model.Cachorro;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 97373f4dddce0df4fd2f286db5e2de1ac7c9faed
 public class DataCachorro {
     private final String nomeArquivo = "cachorro.ser";
     private final String cachorrosAdotados = "adotados.dat";
