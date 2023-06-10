739d61e056dc8f4f2fa20175f404e7e024e978fb
liseras01 <lis-ls@hotmail.com>
2023-06-10 14:06:29 -0300

    nova tela adm

---
+++ src/controller/ControllerTelaAdm.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class ControllerTelaAdm extends Controller {
+
+    @FXML
+    private TextField loginAdm; // login
+    @FXML
+    private TextField senhaAdm; // senha
+    @FXML
+    private Button btnLogar02;
+
+    
+
+@Override
+    public void initialize(URL location, ResourceBundle resources) { // Inicializar a variável status
+        if ("Administrador".equals(loginAdm.getText())) {
+            if("12345".equals(senhaAdm.getText())){
+            btnLogar02.setOnMouseClicked(event -> {
+                try {
+                    mudarTela("../tela/TelaCadastrarCachorro.fxml", event);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+       
+    }
+    }
+}
+
+    
